6b6b23c3fb4b61cf4b3dc158f2c38f7f3b0a3670
jorgesernaalba <jorseral@gmail.com>
2020-03-03 20:29:28 +0100

    typewriter function (JS)

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,4 +4,23 @@ $(document).ready(function() {
     //quita/pon classe show-sidebar de sidebar
     $(".sidebar").toggleClass("show-sidebar");
   });
+
+  //llamamos a la funcion typeWriter cuando el document es READY (si no no encunetra el elemento HTML)
+  typeWriter();
 });
+
+var i = 0;
+var j = 12;
+var txt = "Jorge Serna"; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    var elementoHTML = document.getElementById("demo");
+    var textoQueHayAhora = elementoHTML.innerHTML;
+
+    elementoHTML.innerHTML = textoQueHayAhora + txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
