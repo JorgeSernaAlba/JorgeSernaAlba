0bf0a2d21a3271ebf0c72a87f6f1fe529cb92007
jorgesernaalba <jorseral@gmail.com>
2020-03-03 20:42:24 +0100

    jquery type effect

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,4 +4,17 @@ $(document).ready(function() {
     //quita/pon classe show-sidebar de sidebar
     $(".sidebar").toggleClass("show-sidebar");
   });
+
+  if ($("#typed").length) {
+    new Typed("#typed", {
+      stringsElement: "#typed-strings",
+      typeSpeed: 90,
+      backSpeed: 0,
+      backDelay: 700,
+      startDelay: 200,
+      fadeOut: !1,
+      loop: !0,
+      showCursor: !1
+    });
+  }
 });
