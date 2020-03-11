09365522a19a1745a241ce2b57a884a4312ae07e
jorgesernaalba <jorseral@gmail.com>
2020-03-11 21:09:10 +0100

    Add CSS animations and jquery for sidebar nav

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     $(".sidebar").toggleClass("show-sidebar");
   });
 
+  //escribir en home
   if ($("#typed").length) {
     new Typed("#typed", {
       stringsElement: "#typed-strings",
@@ -17,4 +18,16 @@ $(document).ready(function() {
       showCursor: !1
     });
   }
+
+  //v = "pt-page-scaleDown",
+  // h = "pt-page-moveFromLeft pt-page-ontop"
+  //animaciones sidebar nav
+  $(".sidebar ul a").click(function() {
+    //esto es el valor de href del sidebar link que has clickado
+    var href = $(this).attr("href");
+
+    var section = $(href);
+    section.addClass("pt-page-moveFromLeft pt-page-ontop");
+    //section.addClass("pt-page-moveFromLeft pt-page-ontop");
+  });
 });
